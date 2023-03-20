d79d6282e0cf56e2d157170e018bca0b012ec0e7
05AM <anytime0224@gmail.com>
2023-03-20 10:08:30 +0900

    create project

---
+++ src/test/java/com/sebel/chatting_webSocket/ChattingWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebel.chatting_webSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
